87d5faa4518b6d3c477bc28313f9407c94bba129
andreamarcauvc <andrea@marcau.vc>
2024-12-12 11:56:15 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 
 const uri = "mongodb+srv://andrea:iJssCWigzoUcsJxi@founderappvfti.l3c3o.mongodb.net/?retryWrites=true&w=majority&appName=FounderAppVFTI";
